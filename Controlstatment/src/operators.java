fdf2f80037b521202422d8045de5306224ecc071
Amit Kumar <amit18csu097@ncuindia.edu>
2019-01-17 09:36:50 +0530

    Adding Files

--- Controlstatment/src/operators.java
+++ Controlstatment/src/operators.java
@@ -1,17 +1,17 @@
 class operators
 {
-	public statis void main(String args[])
+	public static void main(String args[]){
 	int a=2;
 	int b=1;
 	int add,multiply,division,subs,modulas;
-	add=a+b;
-	subs=a-b;
-	multiply=a*b;
-	division=a/b;
-	modulas=a%b;
-	System.out.println(add);
-	System.out.println(subs);
-	System.out.println(multiply);
-	System.out.println(division);
-	System.out.println(modulas);	
-}
+	add=(a+b);
+	subs=(a-b);
+	multiply=(a*b);
+	division=(a/b);
+	modulas=(a%b);
+	System.out.println( add );
+	System.out.println( subs );
+	System.out.println( multiply );
+	System.out.println( division );
+	System.out.println( modulas );	
+}}
